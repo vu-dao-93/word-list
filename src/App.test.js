b8fced583a01e0af9a5c3dd74f2181e1a0acb9a2
Vu Dao <Vu Dao>
2017-01-03 16:49:14 +0200

    fix test & add build folder

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ jest.mock('./apiCall');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { mount } from 'enzyme';
+import apiCall from './__mocks__/apiCall';
 
 import App from './App';
 
@@ -12,15 +13,22 @@ describe('App', () => {
     ReactDOM.render(<App />, div);
   });
 
-  const app = mount(<App/>)
-  app.instance().componentDidMount()
-  app.state('columns').forEach(col => {
-    it(`should sortItems by "${col}"`, () => {
-      app.instance()._sortRows(col);
-
-      const unSortedArray = app.state('rows').map(item => item[col])
-      const sortedArray = unSortedArray.map(item => item).sort()
-      expect(sortedArray).toEqual(unSortedArray)
+  describe('mount component', () => {
+    let app = mount(<App/>)
+    it('should get rows data', () => {
+      setTimeout(function () {
+        expect(app.state().rows.length).toBeGreaterThan(0)
+      }, 10);
+    })
+    it(`should sortItems by columns`, () => {
+      setTimeout(function () {
+        app.state().columns.forEach(col => {
+          app.instance()._sortRows(col);
+          const unSortedArray = app.state('rows').map(item => item[col])
+          const sortedArray = unSortedArray.map(item => item).sort()
+          expect(sortedArray).toEqual(unSortedArray)
+        })
+      }, 10);
     })
   })
 })
